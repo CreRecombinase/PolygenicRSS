ea069194ec39dd8fe54756bb227e187e18b29162
Nicholas Knoblauch <nwknoblauch@gmail.com>
2017-08-09 12:33:05 -0500

    migrating functions to their respective packages

--- code/script.py
+++ code/script.py
@@ -9,8 +9,27 @@
 #
 
 import sys
+import h5py
+import numpy as np
+import pandas as pd
+import feather
 
 args = sys.argv[1:]
+matfile = args[0]
+outfeather = args[1]
+if len(args)==3:
+    groupname = args[2]
+else:
+    groupname='snp_info'
+    
+f= h5py.File(matfile,'r')
+si=f[groupname]
 
-for i in range(len(args)):
-    sys.stdout.write("Argument %d: %s\n"%(i + 1, args[i]))
+tdf=[f[si[i,0]][:] for i in range(3)]
+tdfd={'rsid':[int(z) for z in tdf[0][0]],'ref':[chr(x) for x in tdf[1][0]],'alt':[chr(y) for y in tdf[2][0]]}
+
+my_df=pd.DataFrame(tdfd)
+my_df.to_feather(outfeather)
+
+
+
