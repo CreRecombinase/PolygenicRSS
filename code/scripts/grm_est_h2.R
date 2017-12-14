c6ea1bc8814b6a7f290845ff44101eecaa727c0b
Nicholas Knoblauch <nwknoblauch@gmail.com>
2017-12-14 13:14:26 -0600

    move H5 export to gardner

--- code/scripts/grm_est_h2.R
+++ code/scripts/grm_est_h2.R
@@ -5,26 +5,47 @@ library(readr)
 
 xf <- snakemake@input[["gdsf"]]
 yf <-snakemake@input[["h5f"]]
-of <- snakemake@output[["gremef"]]
-
+of <- snakemake@output[["grmef"]]
+stopifnot(!is.null(of))
 
 X <- scale(t(RcppEigenH5::read_2d_mat_h5(xf,"/","dosage")),center=T,scale=F)
 ym <- scale(RcppEigenH5::read_2d_mat_h5(yf,"trait","ymat"),center=T,scale=F)
 si <- RcppEigenH5::read_df_h5(yf,"SimulationInfo")
+sx <- sum(apply(X,2,var))
+p <- ncol(X)
+svdX <- svd(X/sqrt(p))
 
+U <- svdX$u
+D <- svdX$d
+rm(svdX,X)
+gc()
 
-optim_pg <- function(par,X,y){
-    ret <-polygenic.model(X = X,y = y,h = par[1])
-    gc()
+optim_pg <- function(par,D,U,y,sx,p){
+    ret <- polygenic.model_svd(U = U,D=D,y = y,h = par[1],sx=sx,p=p)
     return(-ret$logw)
 }
 
-all_res_df <- apply(ym,2,function(yv,x){
-    optimise(optim_pg,interval = c(1e-4,1),X=X,y=ym)
-},x=X) %>%
-    map_df(as_data_frame) %>%
-    mutate(fgeneid=as.character(1:n())) %>%
+## optim_cg <- function(par,x,y){
+##     ret <- polygenic.model(X=x,y=y,h=par[1])
+##     return(-ret$logw)
+## }
+    
+
+
+## tyml <- yml[1:5]
+## p_res <- tyml %>% imap_dfr(function(yv,fgeneid,X){
+##     optimise(optim_cg,interval=c(1e-4,1),x=X,y=yv) %>%
+##         as_data_frame() %>% mutate(fgeneid=as.character(fgeneid))
+## },X=X) %>% inner_join(si)
+
+
+all_res_df <- array_branch(ym,margin=2) %>% imap_dfr(function(yv,fgeneid,U,D,sx,p){
+    optimise(optim_pg,interval = c(1e-4,1),U=U,D=D,sx=sx,p=p,y=yv) %>%
+        as_data_frame() %>% mutate(fgeneid=as.character(fgeneid))
+},U=U,D=D,sx=sx,p=p) %>%
     inner_join(si)
+
+
 
 write_delim(all_res_df,path = of,delim="\t")
 
--- code/scripts/subset_wtcc.R
+++ code/scripts/subset_wtcc.R
@@ -1,9 +1,10 @@
 library(SeqSupport)
 library(tidyverse)
-inf <- "/media/nwknoblauch/Data/combined.h5"
-rhdf5::h5ls(inf)
+inf <- snakemake@input[["input_h5"]]
+outf <- snakemake@output[["output_h5"]]
+gdsf <- snakemake@input[["input_gds"]]
+gn <-snakemake@parms[["gn"]]
 
-gdsf <- "~/Desktop/scratch/polyg_scratch/all_gds/ALL_seq_hapmap_geno.gds"
 gds <- SeqArray::seqOpen(gdsf)
 si_map <- read_SNPinfo_gds(gds,alleles=T,map=T) %>% separate(allele,into=c("minor","major"))
 
@@ -13,6 +14,7 @@ snp_info_f <- function(file_name,grp_name,col_names=c("chr","labels","major","mi
     mutate(mat_snp_id=1:n(),grp=grp_name)
 }
 gwas_names <- c("bd","cad","cd","ht","ra","t1d","t2d")
+stopifnot(any(gn %in% gwas_names))
 
 snp_info <- map(gwas_names,snp_info_f,file_name=inf) %>% map(function(x){
   mutate(x,labels=paste0("rs",as.integer(labels)),major=R.oo::intToChar(major),minor=R.oo::intToChar(minor),pos=as.integer(pos),chr=as.character(chr)) %>%
@@ -21,28 +23,24 @@ snp_info <- map(gwas_names,snp_info_f,file_name=inf) %>% map(function(x){
 yl <-  map(gwas_names,snp_info_f,file_name=inf,col_names="y") %>% set_names(gwas_names)
 sum(map_dbl(yl,nrow))
 
-
 inter_snpsl<- map(snp_info,semi_join,y=si_map,by=c("SNP","chr"))
 
 inter_snpsl <- map(inter_snpsl,function(x){select(x,-mat_snp_id,-grp)})
 inter_snps <- reduce(inter_snpsl,inner_join)
 
-sub_snp_info <- map(snp_info,semi_join,y=inter_snps)
-
-noutf <- "/media/nwknoblauch/Data/combined_harmonized.h5"
+sub_snp_info <- map(snp_info,semi_join,y=inter_snps) %>% set_names(gwas_names)
 map_si <- select(si_map,SNP,chr,map)
+
+input_file <- inf
+output_file <- outf
 subset_write <- function(df,input_file,output_file,map_si){
   grp_name <- unique(df$grp)
   stopifnot(length(grp_name)==1)
   subset_id <- df$mat_snp_id
   df <- inner_join(df,map_si)
   sub_X <- rhdf5::h5read(input_file,paste0(grp_name,"/X"))[,subset_id]
-  gc()
-
-
-
-
-
+  RcppEigenH5::write_mat_h5(output_file,"dosage",sub_X,deflate_level=4L,data = sub_X)
+  write_df_h5(df,groupname="SNPinfo")
 }
-iwalk(gwas_names)
+subset_write(sub_snp_info[[gn]],inf,outf,map_si)
 
