d96f6927a6d7d2be9c9a5fcd9a7d2b9535379e6d
CreRecombinase <nwknoblauch@midway2-login2.rcc.local>
2018-02-27 15:13:36 -0600

    tweaked rcc config

--- code/scripts/map2RDS.R
+++ code/scripts/map2RDS.R
@@ -12,7 +12,7 @@ map_file_df <- data_frame(map_file = map_files,
 map_df <- map2_dfr(map_file_df$map_file, map_file_df$chr,
                    function(filename, chrom){
                        read_delim(filename,
-                                  col_names = c("SNP", "pos", "map"),
+                                  col_names = c( "pos", "map"),
                                   delim = " ",col_types=c("_in")) %>% mutate(chr = chrom)
                    })
 
