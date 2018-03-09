285de3d0404e956a23aad691d24cf3fa88c87058
CreRecombinase <nwknoblauch@gmail.com>
2018-03-09 15:23:47 -0600

    got rid of a bunch of old scripts I wasn't using anymore

--- code/scripts/evd_chrom.R
+++
@@ -1,8 +0,0 @@
-library(SeqSupport)
-
-
-hdf_f <- snakemake@input[["hdff"]]
-chrom <- as.character(snakemake@params[["chrom"]])
-cutoff <- as.numeric(as.character(snakemake@params[["cutoff"]]))
-
-
--- code/scripts/write_h5_snpinfo_txt.R
+++
@@ -1,8 +0,0 @@
-library(SeqSupport)
-library(tidyverse)
-
-inputf <- snakemake@input[["input_h5"]]
-outputf <- snakemake@output[["output_df"]]
-stopifnot(!is.null(inputf),!is.null(outputf))
-
-read_df_h5(inputf,"SNPinfo") %>% write_delim(outputf,delim="\t")
