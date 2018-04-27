4fe61f4eb74b72f5439c3faeba473fce2d7e5586
CreRecombinase <nwknoblauch@gmail.com>
2018-04-27 14:27:13 -0500

    tweaked imputation pipeline

--- code/snakemake_files/submit_gardner.sh
+++ code/snakemake_files/submit_gardner.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-#--output={cluster.output} --error={cluster.error}
-#--job-name={cluster.jobname}
 snakemake --verbose -j $1  --cluster-config gardner.yaml --cluster "qsub -V -l walltime={cluster.time} -l nodes={cluster.nodes}:ppn={cluster.cpus} -l mem={cluster.mem}"
