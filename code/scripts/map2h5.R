475f436abd6ecb4f7d797f2a7e6f95b3794bc0f1
nwknoblauch <nwknoblauch@gmail.com>
2018-07-01 16:36:36 -0500

    add map2h5

---
+++ code/scripts/map2h5.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(LDshrink)
+library(EigenH5)
+
+
+pop <- "CEU"
+outf <- snakemake@output[["mapf"]]
+pop  <- snakemake@params[["pop"]]
+
+destination <- tempfile()
+base_url <- "ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/technical/working/20130507_omni_recombination_rates/"
+download.file(paste0(base_url, pop, "_omni_recombination_20130507.tar"), destfile = destination)
+destination_dir  <- tempdir()
+untar(destination, exdir=destination_dir)
+## result_files <- , full.names = T)
+
+map_file_df <- data_frame(filename=dir(file.path(destination_dir, pop), full.names=T)) %>%
+    mutate(chrom=as.integer(gsub(paste0(pop, "-([0-9]+)-final.txt.gz"), "\\1", basename(filename))))
+  map_df <- pmap_dfr(map_file_df, function(filename, chrom){
+  read_delim(filename, delim="\t", col_names=c("pos", "rate", "map", "filtered"), skip = 1, trim_ws = T) %>% mutate(chr=chrom)
+  }) %>% arrange(chr, pos) %>% select(chr, map, pos)
+data(map_parameters)
+write_df_h5(map_df, "SNPinfo", outf)
