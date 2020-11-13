742bfb89746fc57ef55909a31d102d0033af4eab
CreRecombinase <nwknoblauch@gmail.com>
2020-11-13 17:36:53 -0600

    drop gz

--- scripts/sumstat2db.R
+++ scripts/sumstat2db.R
@@ -6,7 +6,8 @@ gwasf <- snakemake@input[["gwasf"]]
 output_d <- snakemake@output[["output_d"]]
 
 con = dbConnect(duckdb::duckdb(), dbdir=output_d,read_only=FALSE)
-sumstat_df <- vroom::vroom(gwasf) %>%
+sumstat_df <-  vroom::vroom(gwasf) %>%  
+    dplyr::transmute(SNP=SNP,A1=A1,A2=A2,N=N,Z=BETA/SE) %>%
     mutate(rsid = rsid2int(SNP))
 
 dbWriteTable(con,"gwas",sumstat_df)
